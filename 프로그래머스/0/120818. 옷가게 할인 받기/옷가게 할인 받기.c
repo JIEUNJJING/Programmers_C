f17b00ac440660ec11527d18b9c3106a6f416e0e
Park Ji Eun <mo111b@naver.com>
2025-01-05 15:00:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price) {
+
+    if (price >= 500000)
+        return price - (price * 0.2);
+    else if (price >= 300000)
+        return price - (price * 0.1);
+    else if (price >= 100000)
+        return price - (price * 0.05);
+    else
+        return price;
+}
