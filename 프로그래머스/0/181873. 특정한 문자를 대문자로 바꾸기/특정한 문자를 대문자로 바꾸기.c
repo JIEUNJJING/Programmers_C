5af23cd3a5d20b0a0d46e8c617e1cc5b47b23be7
Park Ji Eun <mo111b@naver.com>
2025-01-22 14:49:35 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.c
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.c
@@ -8,7 +8,7 @@ char* solution(const char* my_string, const char* alp) {
     char* answer = (char*)malloc((strlen(my_string) + 1) * sizeof(char));
     int j = 0;
     for (int i = 0; i < strlen(my_string); i++) {
-        if (my_string[i] != alp[0])
+        if (my_string[i] != *alp)
             answer[j++] = my_string[i];
         else
             answer[j++] = toupper(my_string[i]);
