514e4be1eefb364ba5d607a2a6d23d3550f2f4f8
Park Ji Eun <mo111b@naver.com>
2025-05-12 21:59:08 +0900

    [Bronze III] Title: 네 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/10824. 네 수/네 수.c
+++ 백준/Bronze/10824. 네 수/네 수.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main() {
-    int a, b, c, d;
-    long long num1, num2;
-    char str1[20], str2[20];
 
-    scanf("%d %d", &a, &b);
-    sprintf(str1, "%d%d", a, b);
-    scanf("%d %d", &c, &d);
-    sprintf(str2, "%d%d", c, d);
-    
-    num1 = atoll(str1);
-    num2 = atoll(str2);
-    
-    printf("%lld", num1 + num2);
-    
-    return 0;
+	int A, B, C, D;
+	char num1[20], num2[20];
+    long long r1, r2;
+
+	scanf("%d %d %d %d", &A, &B, &C, &D);
+	sprintf(num1, "%d%d", A, B);
+	sprintf(num2, "%d%d", C, D);
+
+	r1 = atoll(num1);
+	r2 = atoll(num2);
+
+	printf("%lld\n", r1 + r2);
+
+	return 0;
 }
